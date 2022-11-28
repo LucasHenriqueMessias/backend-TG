2fb95ba048cafdd9d4020b69f980ea1099b0a1b3
Lucas Henrique Messias Gonçalves <64761016+LucasHenriqueMessias@users.noreply.github.com>
2022-11-28 20:18:10 -0300

    Delete src directory

--- src/main/java/com/ads/farmacia/servicos/execoes/EntidadeNaoEncontrada.java
+++
@@ -1,9 +0,0 @@
-package com.ads.farmacia.servicos.execoes;
-
-public class EntidadeNaoEncontrada extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-
-	public EntidadeNaoEncontrada(String msg) {
-		super(msg);
-	}
-}
